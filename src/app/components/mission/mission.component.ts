c14b2dc99797f995fa1d7ba841f27c7d3e7b4c8e
TiagoAltstadt <tiago@qrsolutions.com.ar>
2022-12-26 20:22:28 -0300

    update/ Added mission content and a pretty dynamic background

--- src/app/components/mission/mission.component.ts
+++ src/app/components/mission/mission.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mission',
   templateUrl: './mission.component.html',
-  styleUrls: ['./mission.component.scss']
+  styleUrls: ['./mission.component.scss'],
 })
-export class MissionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class MissionComponent {}
--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -39,12 +39,12 @@ export class MainLayoutComponent implements OnInit {
       label: 'Contactanos',
       url: '/contact',
     },
-    {
-      id: 4,
-      icon: 'user',
-      label: 'Usuario',
-      url: '/profile',
-    },
+    // {
+    //   id: 4,
+    //   icon: 'user',
+    //   label: 'Usuario',
+    //   url: '/profile',
+    // },
   ];
   constructor(private router: Router) {}
 
