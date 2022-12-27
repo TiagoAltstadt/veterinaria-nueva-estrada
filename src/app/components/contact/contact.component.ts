c8a7f0b62e7e8c890d777431446afa8bcf1a29b6
TiagoAltstadt <tiago@qrsolutions.com.ar>
2022-12-27 14:25:30 -0300

    update/Corrected some contact-us descriptions

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -21,7 +21,7 @@ export class ContactComponent implements OnInit {
       link: '',
       title: 'En nuestro local',
       description:
-        'Siempre seras vienvenido para un sobreturno en nuestra veterinaria en Estrada 2958 de Lunes a Viernes de 8 a 18! Recorda que podes sacar turno a travez de Setmore en el inicio de la pagina!',
+        'En nuestra veterinaria, ubicada en Estrada 5824 (Mapa interactivo en Inicio). De Lunes a viernes de 9.30hs a 17hs y Sabados de 10hs a 14hs. Para atencion clinica recomendamos sacar turno desde nuestra plataforma de turnos Setmore (podes encontrarla en Inicio)',
       icon: 'home-alternative',
       color: 'violet',
     },
@@ -30,7 +30,7 @@ export class ContactComponent implements OnInit {
       link: '',
       title: 'Whatsapp',
       description:
-        'Podes contactarnos por whatsapp de Lunes a viernes de 8 a 18 al 2230000000',
+        'Podes contactarnos por whatsapp de Lunes a viernes de 9.30hs a 17hs al +54 9 2235939602. Clickea el Icono!',
       icon: 'whatsapp',
       color: 'green',
     },
@@ -39,7 +39,7 @@ export class ContactComponent implements OnInit {
       link: '',
       title: 'Facebbok',
       description:
-        'No seriamos una Veterinaria seria si no contaramos con nuestra propia pagina de Facebook, podes encontrarnos aca!',
+        'No seriamos una Veterinaria seria si no contaramos con nuestra propia pagina de Facebook, Clickea el icono!',
       icon: 'facebook',
       color: 'blue',
     },
@@ -48,7 +48,7 @@ export class ContactComponent implements OnInit {
       link: '',
       title: 'Instagram',
       description:
-        'Si queres estar al dia con las ultimas novedades, Vete-tips, participar en concursos y tener una fiel mirada de con que es lo que contamos en Veterinaria nueva estrada, Seguinos en instagram como @veterinarianuevaestrada',
+        'Si queres estar al dia con las ultimas novedades, Vete-tips, participar en concursos y tener una fiel mirada de con que es lo que contamos en Veterinaria nueva estrada, Seguinos en instagram como @veterinarianuevaestrada, Clickea el Icono!',
       icon: 'instagram',
       color: 'red',
     },
@@ -57,7 +57,7 @@ export class ContactComponent implements OnInit {
       link: '',
       title: 'Setmore',
       description:
-        'Recorda que podes sacar turnos directamente desde Setmore, un acceso directo de el sistema se encuentra en el inicio de la pagina, sino en Instagram, Facebook o Whatsapp vas a encontrar el link directo. Nunca va a estar de mas avisar que podes llamarnos y uno de nuestros profecionales te va a asignar un turno o darte la auda que necesites por telefono, nos importa mantener viva la calidez humana que nos identifica.',
+        'Recorda que podes sacar turnos directamente desde Setmore, un acceso directo de el sistema se encuentra en el inicio de la pagina, sino en Instagram, Facebook o Whatsapp vas a encontrar el link directo. Nunca va a estar de mas avisar que podes llamarnos y uno de nuestros profecionales te va a asignar un turno o darte la auda que necesites por telefono, nos importa mantener viva la calidez humana que nos identifica. Clickea el Icono!',
       icon: 'calendar',
       color: 'lightblue',
     },
